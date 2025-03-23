1b84345cd9cc1f9a865d29ee2583aec3bde6ee19
石田貴行 <t.ishida0710@gmail.com>
2025-03-23 15:37:49 +0900

    kadai_007修正08

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,14 +1,16 @@
 // JavaScriptのif文参考例です。
-const num = 15;
+const num = 3;
 
-// weight 変数numが3の倍数の場合： “3の倍数です”
-// weight 変数numが5の倍数の場合： “5の倍数です”
-// weight 変数numが3と5の倍数の場合： “3と5の倍数です”
+// 変数numが3の倍数の場合： “3の倍数です”
+// 変数numが5の倍数の場合： “5の倍数です”
+// 変数numが3と5の倍数の場合： “3と5の倍数です”
 
-if (num % 3 == 0 && num % 5 == 0) {
+if (num % 3 == 0) {
   console.log("3の倍数です");
 } else if (num % 5 == 0) {
   console.log("5の倍数です");
-} else {
+} else if (num % 3 == 0 && num % 5 == 0) {
   console.log("3と5の倍数です");
+} else {
+  console.log("");
 }
