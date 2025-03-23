b7baec440aac0eb190ef46bd17f5c99214ad54b2
石田貴行 <t.ishida0710@gmail.com>
2025-03-23 17:00:19 +0900

    kadai_mdn修正07

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const today = new Date();
 {
-  console.log(new Date.today.getFullYear(year + "年"));
-  console.log(new Date.today.getMonth() + 1);
-  console.log(new Date.today.getDate());
+  console.log(new Date() today.getFullYear(year + "年"));
+  console.log(new Date() today.getMonth(month + "月") + 1);
+  console.log(new Date() today.getDate(date + "日"));
 }
