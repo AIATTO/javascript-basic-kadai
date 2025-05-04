ac912251f07372c6053bc9d7f0e8f18526795d23
石田貴行 <t.ishida0710@gmail.com>
2025-05-04 09:59:47 +0900

    swiper修正

--- swiper/script.js
+++ swiper/script.js
@@ -1,15 +1,15 @@
 const swiper = new Swiper(".swiper", {
-  loop: true, // ループさせる
+  loop: true,
   pagination: {
-    el: ".swiper-pagination", // ページネーションを指定
-    clickable: true, // クリック可能に
+    el: ".swiper-pagination",
+    clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next", // 次へボタン
-    prevEl: ".swiper-button-prev", // 前へボタン
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   autoplay: {
-    delay: 3000, // 3秒ごとに自動再生
-    disableOnInteraction: false, // ユーザー操作後も自動再生続行
+    delay: 3000,
+    disableOnInteraction: false,
   },
 });
