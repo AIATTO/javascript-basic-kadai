d604699e8eadc8d87d0e463c724e80f2f229db5a
石田貴行 <t.ishida0710@gmail.com>
2025-05-04 10:08:36 +0900

    swiper修正

--- swiper03/script.js
+++ swiper03/script.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper(".swiper", {
   loop: true,
-  effect: "fade", // フェード切り替え
-  speed: 1500, // ふんわり感を演出
+  effect: "fade",
+  speed: 1500,
   autoplay: {
     delay: 4000,
     disableOnInteraction: false,
